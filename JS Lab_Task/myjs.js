027a3441225ba3c3c635e39d5b6cd6d8d411b0a6
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2021-07-25 17:10:46 +0600

    JS Lab Task

--- JS Lab_Task/myjs.js
+++ JS Lab_Task/myjs.js
@@ -1,47 +1,43 @@
-var g_flag = false;
+var g_flag=false;
 
-function viewOutput(e) {
-    //var op = get("uname").value;
-    var op = e.value;
-
-    get("op").innerHTML = op;
+function viewOutput(e){
+    var op=e.value;
+    get("op").innerHTML=op;
 }
 
-
-function hideinfo() {
-    get("info").style.display = "none";
+function hideinfo(){
+    get("info").style.display="none";
 }
 
-function showinfo() {
-    get("info").style.display = "block";
+function showinfo(){
+    get("info").style.display="block";
 }
 
-function turnOn() {
-    var bulb_i = get("bulb");
-    bulb_i.src = "pic_bulbon.gif";
+function turnOn(){
+    var buld_i=get("bulb");
+    buld_i.src="pic_bulbon.gif";
 }
 
-
-function turnOff() {
-    var bulb_i = get("bulb");
-    bulb_i.src = "pic_bulboff.gif";
+function turnOff(){
+    var buld_i=get("bulb");
+    buld_i.src="pic_bulboff.gif";
 }
 
-function get(id) {
+function get(id){
     return document.getElementById(id);
 }
 
-function viewGoogle() {
-    var g_f = get("g_form");
-    var btn_g = get("btn_g");
-
-    if(g_flag) {
-        g_f.style.display = "none";
-        btn_g.innerHTML = "Login with Google";
-        g_flag = false;
-    } else {
-        g_f.style.display = "block";
-        btn_g.innerHTML = "Hide Google Login";
-        g_flag = true;
+function viewGoogle(){
+    var g_f=get("g_form");
+    var btn_g=get("btn_g");
+    if(g_flag){
+        g_f.style.display="none";
+        btn_g.innerHTML="Login With Google";
+        g_flag=false;
+    }
+    else{
+        g_f.style.display="block";
+        btn_g.innerHTML="Hide Google Login";
+        g_flag=true;
     }
 }
